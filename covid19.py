c2b3fe3141f6befc71a75399de1b6bbf5d49372b
chisoftltd <chisoftltd@gmail.com>
2021-03-19 15:46:34 +0000

    Changing initial Lat aand Long

--- covid19.py
+++ covid19.py
@@ -7,7 +7,7 @@
 # cdf = by_country.nlargest(n, 'Confirmed')[['Confirmed']]
 
 # Function that will return the updated data frame
-def find_top_confirmed(n = 15):
+def find_top_confirmed(n = 25):
 
   import pandas as pd
   corona_df = pd.read_csv('dataset.csv')
@@ -26,14 +26,16 @@ corona_df = pd.read_csv('dataset.csv')
 
 corona_df=corona_df.dropna()
 
-m=folium.Map(location=[34.223334,-82.461707],
-            tiles='Stamen toner',
-            zoom_start=8)
+m=folium.Map(location=[40.7672726,-73.97152637],
+            tiles='Stamen Terrain',
+            zoom_start=6.5)
 
 def circle_maker(x):
     folium.Circle(location=[x[0],x[1]],
                  radius=float(x[2])*10,
-                 color="red",
+                 color="blue",
+                 fill=True,
+                 fill_color='skyblue',
                  popup='{}\n confirmed cases:{}'.format(x[3],x[2])).add_to(m)
 corona_df[['Lat','Long_','Confirmed','Combined_Key']].apply(lambda x:circle_maker(x),axis=1)
 
